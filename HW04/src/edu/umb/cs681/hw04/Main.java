5440b1235463b5f31813e3ecfaa082bbe07f96a9
RohitVarma951 <100976048+RohitVarma951@users.noreply.github.com>
2023-03-19 18:55:04 -0500

    Add files via upload

---
+++ HW04/src/edu/umb/cs681/hw04/Main.java
@@ -0,0 +1,22 @@
+package edu.umb.cs681.hw04;
+
+import java.nio.file.*;
+import java.util.*;
+import java.util.stream.*;
+
+public class Main {
+    public static void main(String[] args){
+        Path path = Paths.get("src/edu/umb/cs681/hw04/bos-housing.csv");
+        try( Stream<String> lines = Files.lines(path) ){
+            List<List<String>> matrix = lines.map( line -> Stream.of( line.split(",") )
+                .collect( Collectors.toList() ))
+                    .collect( Collectors.toList() );
+            DataPro.nearCharles(matrix);
+            DataPro.lessCrime(matrix);
+            DataPro.processingByAge(matrix);
+            DataPro.roomsAndDist(matrix);
+        } catch (Exception  ex) {
+            System.out.println("failed due to exception\n"+ex.getMessage());
+        }
+    }
+}
